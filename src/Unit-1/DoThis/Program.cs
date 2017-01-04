786c7d5a94d55d0eb3d1e6d79a92922eb32c673b
Petar Jerinic <petarjerinic@live.com>
2017-01-04 11:03:55 +0100

    Unit1, Lesson3

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -1,30 +1,36 @@
-﻿using System;
 ﻿using Akka.Actor;
 
 namespace WinTail
 {
-    #region Program
-    class Program
-    {
-        public static ActorSystem MyActorSystem;
+	#region Program
 
-        static void Main(string[] args)
-        {
-            // initialize MyActorSystem
+	internal class Program
+	{
+		public static ActorSystem MyActorSystem;
+
+		private static void Main(string[] args)
+		{
+			// initialize MyActorSystem
 			MyActorSystem = ActorSystem.Create("MyActorSystem");
-	        
 
 			// time to make your first actors!
-			var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()), nameof(ConsoleWriterActor));
-			var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)), nameof(ConsoleReaderActor));
+			var consoleWriterProps = Props.Create<ConsoleWriterActor>();
+			var consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
 
+			//var validationActorProps = Props.Create<ValidationActor>(consoleWriterActor);
+			var validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
+			var validationActor = MyActorSystem.ActorOf(validationActorProps, "validationActor");
+
+			var consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
+			var consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
 
 			// tell console reader to begin
 			consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
 			// blocks the main thread from exiting until the actor system is shut down
 			MyActorSystem.WhenTerminated.Wait();
-        }
-    }
-    #endregion
+		}
+	}
+
+	#endregion Program
 }
