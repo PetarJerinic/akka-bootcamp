fab8a491e0c24e4a428c10f77af0e2813a6679cc
Petar Jerinic <petarjerinic@live.com>
2017-01-04 16:21:02 +0100

    Unit2, Lesson2

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms.DataVisualization.Charting;
 using Akka.Actor;
 
 namespace ChartApp.Actors
 {
-    public class ChartingActor : UntypedActor
+    public class ChartingActor : ReceiveActor
     {
-        #region Messages
+		#region Messages
 
-        public class InitializeChart
+		/// <summary>
+		/// Add a new <see cref="Series"/> to the chart
+		/// </summary>
+		public class AddSeries
+		{
+			public AddSeries(Series series)
+			{
+				Series = series;
+			}
+
+			public Series Series { get; private set; }
+		}
+
+		public class InitializeChart
         {
             public InitializeChart(Dictionary<string, Series> initialSeries)
             {
@@ -33,20 +47,23 @@ namespace ChartApp.Actors
         {
             _chart = chart;
             _seriesIndex = seriesIndex;
-        }
 
-        protected override void OnReceive(object message)
-        {
-            if (message is InitializeChart)
-            {
-                var ic = message as InitializeChart;
-                HandleInitialize(ic);
-            }
-        }
+			Receive<InitializeChart>(ic => HandleInitialize(ic));
+			Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
+		}
 
-        #region Individual Message Type Handlers
+	   #region Individual Message Type Handlers
 
-        private void HandleInitialize(InitializeChart ic)
+		private void HandleAddSeries(AddSeries series)
+		{
+			if (!string.IsNullOrEmpty(series.Series.Name) && !_seriesIndex.ContainsKey(series.Series.Name))
+			{
+				_seriesIndex.Add(series.Series.Name, series.Series);
+				_chart.Series.Add(series.Series);
+			}
+		}
+
+		private void HandleInitialize(InitializeChart ic)
         {
             if (ic.InitialSeries != null)
             {
