2da870c9f248d1cedcbbc2e42df40e70fb29e460
Petar Jerinic <petarjerinic@live.com>
2017-01-05 10:27:21 +0100

    Unit2, Lesson3 and Unit2, Lesson4

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -1,15 +1,31 @@
-﻿using System;
+﻿using Akka.Actor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
-using Akka.Actor;
 
 namespace ChartApp.Actors
 {
-    public class ChartingActor : ReceiveActor
-    {
+	public class ChartingActor : ReceiveActor
+	{
+		/// <summary>
+		/// Maximum number of points we will allow in a series
+		/// </summary>
+		public const int MaxPoints = 250;
+
+		/// <summary>
+		/// Incrementing counter we use to plot along the X-axis
+		/// </summary>
+		private int xPosCounter = 0;
+
 		#region Messages
+
+		// Actors/ChartingActor.cs - add inside the Messages region
+		/// <summary>
+		/// Toggles the pausing between charts
+		/// </summary>
+		public class TogglePause { }
 
 		/// <summary>
 		/// Add a new <see cref="Series"/> to the chart
@@ -24,68 +40,180 @@ namespace ChartApp.Actors
 			public Series Series { get; private set; }
 		}
 
-		public class InitializeChart
-        {
-            public InitializeChart(Dictionary<string, Series> initialSeries)
-            {
-                InitialSeries = initialSeries;
-            }
+		/// <summary>
+		/// Remove an existing <see cref="Series"/> from the chart
+		/// </summary>
+		public class RemoveSeries
+		{
+			public RemoveSeries(string seriesName)
+			{
+				SeriesName = seriesName;
+			}
 
-            public Dictionary<string, Series> InitialSeries { get; private set; }
-        }
-
-        #endregion
-
-        private readonly Chart _chart;
-        private Dictionary<string, Series> _seriesIndex;
-
-        public ChartingActor(Chart chart) : this(chart, new Dictionary<string, Series>())
-        {
-        }
-
-        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex)
-        {
-            _chart = chart;
-            _seriesIndex = seriesIndex;
-
-			Receive<InitializeChart>(ic => HandleInitialize(ic));
-			Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
+			public string SeriesName { get; private set; }
 		}
 
-	   #region Individual Message Type Handlers
-
-		private void HandleAddSeries(AddSeries series)
+		public class InitializeChart
 		{
-			if (!string.IsNullOrEmpty(series.Series.Name) && !_seriesIndex.ContainsKey(series.Series.Name))
+			public InitializeChart(Dictionary<string, Series> initialSeries)
 			{
-				_seriesIndex.Add(series.Series.Name, series.Series);
-				_chart.Series.Add(series.Series);
+				InitialSeries = initialSeries;
+			}
+
+			public Dictionary<string, Series> InitialSeries { get; private set; }
+		}
+
+		#endregion Messages
+
+		private readonly Chart _chart;
+		private Dictionary<string, Series> _seriesIndex;
+		private readonly Button _pauseButton;
+
+		public ChartingActor(Chart chart, Button pauseButton) :
+			this(chart, new Dictionary<string, Series>(), pauseButton)
+		{
+		}
+
+		public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex,
+			Button pauseButton)
+		{
+			_chart = chart;
+			_seriesIndex = seriesIndex;
+			_pauseButton = pauseButton;
+			Charting();
+		}
+
+		private void Charting()
+		{
+			Receive<InitializeChart>(ic => HandleInitialize(ic));
+			Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
+
+			Receive<RemoveSeries>(removeSeries => HandleRemoveSeries(removeSeries));
+			Receive<Metric>(metric => HandleMetrics(metric));
+
+			Receive<TogglePause>(pause =>
+			{
+				SetPauseButtonText(true);
+				BecomeStacked(Paused);
+			});
+		}
+
+		private void Paused()
+		{
+			Receive<Metric>(metric => HandleMetricsPaused(metric));
+			Receive<TogglePause>(pause =>
+			{
+				SetPauseButtonText(false);
+				UnbecomeStacked();
+			});
+		}
+
+		#region Individual Message Type Handlers
+
+		private void HandleMetricsPaused(Metric metric)
+		{
+			if (!string.IsNullOrEmpty(metric.Series)
+				&& _seriesIndex.ContainsKey(metric.Series))
+			{
+				var series = _seriesIndex[metric.Series];
+				// set the Y value to zero when we're paused
+				series.Points.AddXY(xPosCounter++, 0.0d);
+				while (series.Points.Count > MaxPoints) series.Points.RemoveAt(0);
+				SetChartBoundaries();
 			}
 		}
 
 		private void HandleInitialize(InitializeChart ic)
-        {
-            if (ic.InitialSeries != null)
-            {
-                //swap the two series out
-                _seriesIndex = ic.InitialSeries;
-            }
+		{
+			if (ic.InitialSeries != null)
+			{
+				// swap the two series out
+				_seriesIndex = ic.InitialSeries;
+			}
 
-            //delete any existing series
-            _chart.Series.Clear();
+			// delete any existing series
+			_chart.Series.Clear();
 
-            //attempt to render the initial chart
-            if (_seriesIndex.Any())
-            {
-                foreach (var series in _seriesIndex)
-                {
-                    //force both the chart and the internal index to use the same names
-                    series.Value.Name = series.Key;
-                    _chart.Series.Add(series.Value);
-                }
-            }
-        }
+			// set the axes up
+			var area = _chart.ChartAreas[0];
+			area.AxisX.IntervalType = DateTimeIntervalType.Number;
+			area.AxisY.IntervalType = DateTimeIntervalType.Number;
 
-        #endregion
-    }
+			SetChartBoundaries();
+
+			// attempt to render the initial chart
+			if (_seriesIndex.Any())
+			{
+				foreach (var series in _seriesIndex)
+				{
+					// force both the chart and the internal index to use the same names
+					series.Value.Name = series.Key;
+					_chart.Series.Add(series.Value);
+				}
+			}
+
+			SetChartBoundaries();
+		}
+
+		private void HandleAddSeries(AddSeries series)
+		{
+			if (!string.IsNullOrEmpty(series.Series.Name) &&
+				!_seriesIndex.ContainsKey(series.Series.Name))
+			{
+				_seriesIndex.Add(series.Series.Name, series.Series);
+				_chart.Series.Add(series.Series);
+				SetChartBoundaries();
+			}
+		}
+
+		private void HandleRemoveSeries(RemoveSeries series)
+		{
+			if (!string.IsNullOrEmpty(series.SeriesName) &&
+				_seriesIndex.ContainsKey(series.SeriesName))
+			{
+				var seriesToRemove = _seriesIndex[series.SeriesName];
+				_seriesIndex.Remove(series.SeriesName);
+				_chart.Series.Remove(seriesToRemove);
+				SetChartBoundaries();
+			}
+		}
+
+		private void HandleMetrics(Metric metric)
+		{
+			if (!string.IsNullOrEmpty(metric.Series) &&
+				_seriesIndex.ContainsKey(metric.Series))
+			{
+				var series = _seriesIndex[metric.Series];
+				series.Points.AddXY(xPosCounter++, metric.CounterValue);
+				while (series.Points.Count > MaxPoints) series.Points.RemoveAt(0);
+				SetChartBoundaries();
+			}
+		}
+
+		#endregion Individual Message Type Handlers
+
+		private void SetChartBoundaries()
+		{
+			double maxAxisX, maxAxisY, minAxisX, minAxisY = 0.0d;
+			var allPoints = _seriesIndex.Values.SelectMany(series => series.Points).ToList();
+			var yValues = allPoints.SelectMany(point => point.YValues).ToList();
+			maxAxisX = xPosCounter;
+			minAxisX = xPosCounter - MaxPoints;
+			maxAxisY = yValues.Count > 0 ? Math.Ceiling(yValues.Max()) : 1.0d;
+			minAxisY = yValues.Count > 0 ? Math.Floor(yValues.Min()) : 0.0d;
+			if (allPoints.Count > 2)
+			{
+				var area = _chart.ChartAreas[0];
+				area.AxisX.Minimum = minAxisX;
+				area.AxisX.Maximum = maxAxisX;
+				area.AxisY.Minimum = minAxisY;
+				area.AxisY.Maximum = maxAxisY;
+			}
+		}
+
+		private void SetPauseButtonText(bool paused)
+		{
+			_pauseButton.Text = string.Format("{0}", !paused ? "PAUSE ||" : "RESUME ->");
+		}
+	}
 }
